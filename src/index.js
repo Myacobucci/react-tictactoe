d4624ab59a6bc7173b775d15592fcdac73731052
myacobucci <maxyacobucci@gmail.com>
2019-10-27 20:51:38 -0300

    Calculating winner, end of game

--- src/index.js
+++ src/index.js
@@ -41,6 +41,9 @@ class Board extends React.Component {
 
     handleClick(i){
         const squares = this.state.squares.slice();
+        if (calculateWinner(this.state.squares) || squares[i]){
+            return;
+        }
         squares[i] = this.state.value;
         if (this.state.xIsNext) {
             this.setState({xIsNext: false, value: 'O'})
@@ -51,7 +54,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + this.state.value;
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner
+        } else {
+            status = 'Next player: ' + this.state.value;
+        }
 
         return (
             <div>
@@ -93,6 +102,26 @@ class Game extends React.Component {
 }
 
 // ========================================
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
 ReactDOM.render(
     <Game />,
